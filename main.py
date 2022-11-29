d81cae3f1694e96a409302309d74919805913aa6
Anton <myworksmail7@gmail.com>
2022-11-29 13:30:35 +0600

    29.11

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-a='fdfd'
-print('hello world')
-for x in range(10):
-    print('ку -ку')
-    print('afdaa')
+class Test:
+    a = 100
+    b = 200
+
+    @classmethod
+    def test_classmethod(cls, n):
+        return cls.a <= n and cls.b <= n
+
+    def __init__(self, x, y):
+        self.x = self.y = 0
+        if self.test_classmethod(x) and self.test_classmethod(y):
+            self.x = x
+            self.y = y
+    def return_data(self):
+        print(self.x, self.y)
+
+    @staticmethod
+    def test_staticmethod(x,y):
+        return x+y
+
